be73087480192937079a33038b7f8aba10ba582f
vickyhawley <vix.hawley@gmail.com>
2022-03-11 12:37:50 +0000

    carousel and card changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './HomeScreen.css'
-import { Container } from 'react-bootstrap';
-import Carous from '../components/Carousel';
-import LatestCards from '../components/LatestCards';
-import AboutMe from '../components/AboutMe';
-
+import React from "react";
+import "./HomeScreen.css";
+import { Container } from "react-bootstrap";
+import Carous from "../components/Carousel";
+import LatestCards from "../components/LatestCards";
+import AboutMe from "../components/AboutMe";
 
 const HomeScreen = () => {
-    return (
-        <>
-        <Container fluid>
-            <Carous />
-            </Container>
-        <LatestCards />
-        <AboutMe />
-            
-        </>
-    )
-}
+  return (
+    <>
+      <Container className="carouselHeight" fluid>
+        <Carous />
+      </Container>
+      <LatestCards />
+      <AboutMe />
+    </>
+  );
+};
 
 export default HomeScreen;
